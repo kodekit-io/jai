7f109a1a1eb35a6357695863030d6baf6c1cb2e5
oglab <n.nurjamal@gmail.com>
2016-11-05 14:49:17 +0700

    Css

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,7 +1,7 @@
 <?php //Navigation ?>
 <nav class="ja-nav">
 	<h1 class="ja-logo light-blue darken-4"><a href="{!! lang_url('') !!}" title="Jakarta Aquarium">Jakarta Aquarium</a></h1>
-	<a href="#" class="uk-visible-small ja-mobmenu" data-uk-toggle="{target:'.ja-mobhide',cls:'uk-hidden-small',animation:'uk-animation-slide-fade, uk-animation-fade'}"><i class="uk-icon-bars"></i></a>
+	<a href="#" class="uk-visible-small ja-mobmenu cyan darken-1" data-uk-toggle="{target:'.ja-mobhide',cls:'uk-hidden-small',animation:'uk-animation-slide-fade, uk-animation-fade'}"><i class="uk-icon-bars"></i></a>
 	<ul class="uk-list ja-nav-main cyan darken-1 ja-mobhide uk-hidden-small">
 		<li><a href="{!! lang_url('about-us') !!}" title="About Us" class="ja-icon-about">About Us</a></li>
 		<li>
