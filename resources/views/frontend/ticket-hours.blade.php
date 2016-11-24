7cb91a88b70a63884dd26d3d157b4f6ce615f6e8
oglab <n.nurjamal@gmail.com>
2016-11-24 14:38:35 +0700

    Gini dulu

--- resources/views/frontend/ticket-hours.blade.php
+++ resources/views/frontend/ticket-hours.blade.php
@@ -36,18 +36,39 @@
                             <input id="date" type="text" data-uk-datepicker="{format:'dddd, DD-MM-YYYY'}">
                         </div>
                         <div class="uk-form-row">
-                            <label for="package">Package</label>
-                            <select name="package">
-                                <option value="1">General Admission</option>
-                                <option value="1">Package A</option>
-                                <option value="1">Package B</option>
-                            </select>
-                        </div>
-                        <div class="uk-form-row">
-                            <label for="visitor"><i class="ja-adult"></i>Visitor</label>
-                            <input id="visitor" type="number" placeholder="0">
-                            {{--<label for="visitorchild" class="uk-margin-left"><i class="ja-child"></i>Babies (<2 years, Free)</label>--}}
-                            {{--<input id="visitorchild" type="number" placeholder="">--}}
+                            <h4 class="cyan-text text-darken-1">Admission Package</h4>
+                            <div class="uk-grid uk-grid-small uk-grid-match" data-uk-grid-match data-uk-grid-margin>
+                                <div class="uk-width-medium-1-3">
+                                    <div class="uk-panel-box cyan darken-1 white-text">
+                                        <h4 class="white-text uk-margin-remove">Basic Admission</h4>
+                                        <div class="jai-submission-info">General Aquarium Tour</div>
+                                        <div class="jai-submission-price">Weekday IDR 200.000</div>
+                                    </div>
+                                    <div class="uk-panel-box jai-submission-order white uk-text-right">
+                                        <input type="number" class="right" value="1">
+                                    </div>
+                                </div>
+                                <div class="uk-width-medium-1-3">
+                                    <div class="uk-panel-box light-blue darken-4 white-text">
+                                        <h4 class="white-text uk-margin-remove">Premium Admission</h4>
+                                        <div class="jai-submission-info">Aquarium Tour, 5D Theater and Printed Photo Souvenir.</div>
+                                        <div class="jai-submission-price">Weekday IDR 265.000</div>
+                                    </div>
+                                    <div class="uk-panel-box jai-submission-order white uk-text-right">
+                                        <input type="number" class="right" value="0">
+                                    </div>
+                                </div>
+                                <div class="uk-width-medium-1-3">
+                                    <div class="uk-panel-box amber darken-1 white-text">
+                                        <h4 class="white-text uk-margin-remove">Exclusive Admission</h4>
+                                        <div class="jai-submission-info">Aquarium Tour, 5D Theater, Shark/Piranha Feeding, Seatrek/Dive and Printed Photo Souvenir.</div>
+                                        <div class="jai-submission-price">Weekday IDR 550.000</div>
+                                    </div>
+                                    <div class="uk-panel-box jai-submission-order white uk-text-right">
+                                        <input type="number" class="right" value="0">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="uk-form-row">
                             <label for="promo"><i class="uk-icon-tag"></i>Promo</label>
@@ -71,7 +92,7 @@
 
             </div>
         </div>
-        <div class="ja-ticket__content uk-margin-bottom">
+        <div class="ja-ticket__content uk-margin-large-bottom">
             <h3 class="light-blue-text text-darken-4">OPENING HOURS</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor crasher malarki requiem predantia incididunt ut labore et dolore magna aliqua. Ut eni ad minim veniam, quisiom nostrud at autumn irure dor in reprehenderit exercitation.</p>
             <ul class="uk-list">
