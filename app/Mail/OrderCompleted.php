a7f1610a1d2621716ab057ab19a846a956d3115b
pashamahardika <github@pashamahardika.com>
2017-01-09 03:00:07 +0000

    update email template

--- app/Mail/OrderCompleted.php
+++ app/Mail/OrderCompleted.php
@@ -37,13 +37,13 @@ class OrderCompleted extends Mailable
         $data['orderId'] = $this->order->id;
         $data['name'] = $this->order->name;
         $data['visitDate'] = Carbon::createFromFormat('Y-m-d', $this->order->visit_date)->format('l, d F Y');
-        $data['total'] = number_format($this->order->total_amount, 2, '.', '');
+        $data['total'] = number_format($this->order->total_amount, 0, ',', '.');
         $data['details'] = $this->order->details;
         $email = $this->view('emails.order-completed', $data);
-//        $tickets = $this->order->galasysTickets;
-//        foreach ($tickets as $ticket) {
-//            $email->attach(public_path($ticket->e_ticket));
-//        }
+        $tickets = $this->order->galasysTickets;
+        foreach ($tickets as $ticket) {
+            $email->attach(public_path($ticket->e_ticket));
+        }
 
         return $email;
     }
