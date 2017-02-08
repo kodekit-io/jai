80977849f923609d1b838d6fa14ede180e442633
Pashamahardika <github@pashamahardika.com>
2017-02-08 07:21:16 +0000

    lang

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -17,6 +17,9 @@ class VerifyCsrfToken extends BaseVerifier
         'doku/review',
         'doku/identify',
 
+        'cimb/result',
+        'cimb/backend',
+
         '*/cimb/result',
         '*/cimb/backend',
 
