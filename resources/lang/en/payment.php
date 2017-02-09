4b88a9a6a2c3a59cce33f9c61f3c3d754ea4a0ed
Pashamahardika <github@pashamahardika.com>
2017-02-09 06:25:50 +0000

    payment result translation

---
+++ resources/lang/en/payment.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'success_title' => 'Your payment has been successful!',
+    'success_content' => 'Thank you, your payment has been successful and your booking is now confirmed. A confirmation email has been sent to :email',
+    'on-hold_title' => 'Your order has been received!',
+    'on-hold_content' => 'Your transaction number is :payment_code. You should receive an order confirmation email at :email',
+    'failed_title' => 'Your payment has failed!',
+    'failed_content' => 'We are unable to process your payment. Please review your payment information and try again, or select different payment method. Make sure to complete the payment within the specified period. If you need further assistance, please contact our customer service :customer_service',
+
+];
---
+++ resources/lang/id/payment.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'success_title' => 'Pembayaran telah berhasil!',
+    'success_content' => 'Terima kasih, pembayaran Anda telah berhasil dan telah dikonfirmasi. Email konfirmasi Anda telah dikirimkan ke :email',
+
+    'on-hold_title' => 'Pesanan Anda telah diterima!',
+    'on-hold_content' => 'Nomor transaksi Anda adalah :payment_code. Anda akan mendapatkan email konfirmasi pesanan di :email',
+
+    'failed_title' => 'Pembayaran Anda telah gagal!',
+    'failed_content' => 'Kami tidak dapat memproses pembayaran Anda. Cek kembali informasi pembayaran Anda dan coba lagi, atau pilihlah metode pembayaran yang berbeda. Pastikan Anda menyelesaikan pembayaran pada tenggat waktu yang telah ditentukan. Jika Anda memerlukan bantuan lebih lanjut, silakan hubungi customer service kami di :customer_service',
+
+];
